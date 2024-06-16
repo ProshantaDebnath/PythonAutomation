f8f13f989e266e74b5c45be2b0d830454a087c91
Proshanta Debnath <73601711+ProshantaDebnath@users.noreply.github.com>
2024-06-16 23:30:35 +0530

    Automation Practice #4

--- PythonSelenium/AutomationPractice4.py
+++ PythonSelenium/AutomationPractice4.py
@@ -11,6 +11,7 @@ driver = webdriver.Chrome(service=service_obj)
 
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
 driver.maximize_window()
+time.sleep(5)
 # Implicitly wait
 driver.implicitly_wait(5)
 driver.find_element(By.XPATH, "//form//a//following-sibling::input").send_keys("ber")
@@ -32,3 +33,20 @@ wait = WebDriverWait(driver, 10)
 wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "promoInfo")))
 assert driver.find_element(By.CLASS_NAME, 'promoInfo').text == "Code applied ..!"
 
+Sum = 0
+itemPrices = driver.find_elements(By.XPATH, "//*[@id='productCartTables']//tbody//tr//td[5]//p")
+for itemPrice in itemPrices:
+    Sum = Sum + int(itemPrice.text)
+
+totalAmount = int(driver.find_element(By.CLASS_NAME, 'totAmt').text)
+
+assert Sum == totalAmount
+
+ExpectedList = ['Cucumber - 1 Kg', 'Raspberry - 1/4 Kg', 'Strawberry - 1/4 Kg']
+ActualList = []
+ActualListData = driver.find_elements(By.XPATH, "//*[@id='productCartTables']//tbody//tr//td[2]//p")
+assert len(ActualListData) > 0
+for data in ActualListData:
+    ActualList.append(data.text)
+
+assert ExpectedList == ActualList
